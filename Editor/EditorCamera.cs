1c3a4b481a5a4e9fbede55e21b7717f93ca104f3
AlexTuduran <alex.tuduran@gmail.com>
2016-12-30 20:09:05 +0200

    First commit

---
+++ Editor/EditorCamera.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using UnityStandardAssets.ImageEffects;
+
+[InitializeOnLoad]
+public class EditorCamera : MonoBehaviour {
+    static public bool aaEnabled = true;
+    static public bool moreAAEnabled = false;
+    static public bool insaneAAEnabled = false;
+
+    static EditorCamera() {
+        SceneView.onSceneGUIDelegate += Delegate;
+    }
+
+    static void Delegate(SceneView sv) {
+        if (!aaEnabled) {
+            return;
+        }
+
+        if (Event.current.type != EventType.Layout) {
+            return;
+        }
+            
+        if (!Camera.main) {
+            return;
+        }
+
+        Camera cam = sv.camera;
+
+        if (cam.GetComponent<Antialiasing>()) {
+        }
+        else {
+            Antialiasing aa = cam.gameObject.AddComponent<Antialiasing>();
+            aa.mode = AAMode.FXAA2;
+
+            if (moreAAEnabled) {
+                Antialiasing aa2 = cam.gameObject.AddComponent<Antialiasing>();
+                aa2.mode = AAMode.FXAA2;
+
+                if (insaneAAEnabled) {
+                    Antialiasing aa3 = cam.gameObject.AddComponent<Antialiasing>();
+                    aa3.mode = AAMode.FXAA2;
+                }
+            }
+        }
+    }
+}
